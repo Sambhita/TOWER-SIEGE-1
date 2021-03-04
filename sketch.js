0fa29ef6eb3121b7d716a3ce4ffec3553d78f719
Sambhita <77112921+Sambhita@users.noreply.github.com>
2021-03-04 08:33:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+
+function setup() {
+  engine  = Engine.create();
+  world = engine.world;
+
+  createCanvas(900,400);
+  ground = new Ground();
+  stand1 = new Stand(380,300,270,10);
+ 
+  //level one
+  block1 = new Block(280,275,30,40);  
+  block2 = new Block(310,275,30,40);
+  block3 = new Block(340,275,30,40);
+  block4 = new Block(370,275,30,40);
+  block5 = new Block(400,275,30,40);
+  block6 = new Block(430,275,30,40);
+  block7 = new Block(460,275,30,40);
+  block8 = new Block(490,275,30,40);
+  //level two
+  block9 = new Block(310,235,30,40);
+  block10 = new Block(340,235,30,40);
+  block11 = new Block(370,235,30,40);
+  block12 = new Block(400,235,30,40);
+  block13 = new Block(430,235,30,40);
+  block14 = new Block(460,235,30,40);
+  //level three
+  block15 = new Block(340,195,30,40);
+  block16 = new Block(370,195,30,40);
+  block17 = new Block(400,195,30,40);
+  block18 = new Block(430,195,30,40);
+  //level four
+  block19 = new Block(370,155,30,40);
+  block20 = new Block(400,155,30,40);
+  //level five
+  block21 = new Block(385,115,30,40);
+
+  //ball  with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new SlingShot(this.ball,{x:200,y:200});
+
+}
+
+function draw() {
+ // background(backgroundImg);  
+ background("white")
+  Engine.update(engine);
+ 
+  strokeWeight(2);
+  stroke(15);
+  stand1.display();
+  
+  stroke(15);
+  fill("#B76CFD")
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  stroke(15)
+  fill("#ff6700")
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+ stroke(15)
+ fill("#FF2281")
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
+    stroke(15)
+    fill("#39FF14")
+  block19.display();
+  block20.display();
+ stroke(15)
+ fill("blue")
+  block21.display();
+
+  text("drag the line and release to hit the crazy boxes",600,250)
+
+  ellipse(ball.position.x,ball.position.y,20);
+  slingShot.display();
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+  slingShot.fly();
+}
